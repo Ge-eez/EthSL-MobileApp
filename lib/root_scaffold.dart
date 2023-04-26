e9ca17d8d8ba7016d8f1f55f55b8f924c5cf56dc
Tewodros M <tedymesfin18@gmail.com>
2023-04-26 22:44:51 +0300

    text to signlanguage page added

---
+++ lib/root_scaffold.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'pages.dart';
+
+class RootScaffold extends StatefulWidget {
+  const RootScaffold({Key? key}) : super(key: key);
+
+  @override
+  State<RootScaffold> createState() => _RootScaffoldState();
+}
+
+class _RootScaffoldState extends State<RootScaffold> {
+  final List<Widget> _screens = [
+    HomePage(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+
+  // final List<IconData> _icons = [Icons.home, Icons.library_music, Icons.person];
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 3,
+        selectedItemColor: Color.fromRGBO(255, 255, 255, 1),
+        currentIndex: _selectedIndex,
+        iconSize: 30,
+        selectedLabelStyle: TextStyle(
+          color: Color.fromRGBO(51, 53, 123, 1),
+        ),
+        unselectedItemColor: Color.fromRGBO(216, 216, 216, 0.651),
+        onTap: (index) => setState(() {
+          _selectedIndex = index;
+        }),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: "Home",
+            backgroundColor: Color.fromRGBO(51, 53, 123, 1),
+            //activeIcon: Icon(Icons.home_rounded, color: Colors.red),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_library_rounded),
+            label: "Lessons",
+            backgroundColor: Color.fromRGBO(51, 53, 123, 1),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_searching_rounded),
+            label: "Practice",
+            backgroundColor: Color.fromRGBO(51, 53, 123, 1),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.golf_course_sharp),
+            label: "Challenges",
+            backgroundColor: Color.fromRGBO(51, 53, 123, 1),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book_rounded),
+            label: "Glossary",
+            backgroundColor: Color.fromRGBO(51, 53, 123, 1),
+          ),
+        ],
+      ),
+    );
+  }
+}
