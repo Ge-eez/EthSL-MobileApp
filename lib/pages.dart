cce648212cca3cb9aebb139995caa4510a8c83b4
Tewodros M <tedymesfin18@gmail.com>
2023-06-03 03:24:41 +0300

    merge conflict solved

--- lib/pages.dart
+++ lib/pages.dart
@@ -4,7 +4,4 @@ export 'homepage/screens/homepage.dart';
 export 'root_scaffold.dart';
 export 'textToSign/text_to_sign_page.dart';
 export 'profile/editprofile.dart';
-export 'profile/signin.dart';
-export 'profile/signup.dart';
 export 'lessons/lessons_page.dart';
-
